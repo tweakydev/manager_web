155f5471afa80a3f30f491f806511305a0d143b6
callum <62078259+juleswhi@users.noreply.github.com>
2026-09-12 02:51:04 +0100

    add gitignore

--- obj/Debug/net9.0/ManagerWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/ManagerWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagerWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17aefb197fc4126cb2ab3b09cc280b45ee794e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagerWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagerWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
